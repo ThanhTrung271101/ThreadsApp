2ffa0f7882f3bbaff69626dca6a400bfcf0da611
Thành Trung <npttrung2711@gmail.com>
2024-09-01 21:58:46 +0700

    Initial Commit

---
+++ ThreadsApp/App/ThreadsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsAppApp.swift
+//  ThreadsApp
+//
+//  Created by Thành Trung on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThreadsApp/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadsApp
+//
+//  Created by Thành Trung on 1/9/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
