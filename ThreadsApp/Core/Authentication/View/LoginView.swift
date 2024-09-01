60c77040cefdaa88d7f66ff610aa1b0fad771e43
Thành Trung <npttrung2711@gmail.com>
2024-09-01 23:12:12 +0700

    + authentication UI

--- ThreadsApp/Core/Authentication/View/LoginView.swift
+++ ThreadsApp/Core/Authentication/View/LoginView.swift
@@ -8,8 +8,71 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                
+                Spacer()
+                
+                Image("threads-app-icon")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 120, height: 120)
+                    .padding()
+                
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .textInputAutocapitalization(.never)
+                        .modifier(ThreadsTextFieldModifier())
+                    TextField("Enter your password", text: $password)
+                        .modifier(ThreadsTextFieldModifier())
+                }
+                
+                NavigationLink {
+                    Text("Forgot password?")
+                } label: {
+                    Text("Forgot password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.vertical)
+                        .padding(.trailing, 28)
+                        .foregroundStyle(Color(.black))
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(Color(.white))
+                        .frame(width: 352, height: 44)
+                        .background(Color(.black))
+                        .clipShape(Capsule())
+                }
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    RegistrationView()
+                } label: {
+                    HStack(spacing: 3 ) {
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                    .foregroundStyle(Color(.black))
+                }
+                .padding(.vertical, 14)
+
+            }
+        }
     }
 }
 
---
+++ ThreadsApp/Utils/ViewModifiers/ThreadsTextFieldModifier.swift
@@ -0,0 +1,19 @@
+//
+//  ThreadsTextFieldModifier.swift
+//  ThreadsApp
+//
+//  Created by Thành Trung on 1/9/24.
+//
+
+import SwiftUI
+
+struct ThreadsTextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding(.horizontal, 24)
+    }
+}
